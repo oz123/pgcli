2551b643f6f78e3f12a2058ef2c0aabe30002359
Oz Tiram <oz.tiram@gmail.com>
2024-08-16 09:04:08 +0200

    add script to build in docker

---
+++ script-in-docker.sh
@@ -0,0 +1,15 @@
+dnf install 'dnf-command(config-manager)'
+dnf install 'dnf-command(config-manager)'
+dnf config-manager --set-enabled powertools
+
+dnf install epel-release
+
+dnf install -y gcc make python3-devel python3-pip
+dnf install -y python3.8
+dnf install -y patchelf
+dnf install -y postgresql-devel
+python3.8 -m venv venv
+source venv/bin/activate
+pip install --upgrade pip
+
+pip install pgspecial zstandard configobj "cli_helpers[styles]" Nuitka  "psycopg[c]" cython python38-devel python38-Cython prompt_toolkit pygments 
