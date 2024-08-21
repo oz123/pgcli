7fb3c9d9ddeb35ec24fbe3f767d23d269aebdc4e
Oz Tiram <oz.tiram@gmail.com>
2024-08-21 17:18:18 +0200

    install python3.8 headers with dnf

--- script-in-docker.sh
+++ script-in-docker.sh
@@ -5,11 +5,11 @@ dnf config-manager --set-enabled powertools
 dnf install -y epel-release
 
 dnf install -y gcc make python3-devel python3-pip
-dnf install -y python3.8
+dnf install -y python3.8 python38-devel python38-pip python38-Cython
 dnf install -y patchelf
 dnf install -y postgresql-devel
 python3.8 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
 
-pip install pgspecial zstandard configobj "cli_helpers[styles]" Nuitka  "psycopg[c]" cython python38-devel python38-Cython prompt_toolkit pygments 
+pip install pgspecial zstandard configobj "cli_helpers[styles]" Nuitka  "psycopg[c]" prompt_toolkit pygments 
