0a121dce8cc73768d378ac73953c2f026e5c7036
Oz Tiram <oz.tiram@gmail.com>
2024-08-22 21:04:39 +0200

    Linux -> linux

--- script-in-docker-alpine.sh
+++ script-in-docker-alpine.sh
@@ -7,6 +7,6 @@ source venv/bin/activate
 pip install --upgrade pip
 pip install pgspecial zstandard configobj "cli_helpers[styles]" Nuitka  "psycopg[c]" prompt_toolkit pygments 
 
-nuitka --show-progress --onefile -o pgcli-$(uname -o | cut -d / -f 2)-musl-$(uname -m).exe pgcli --python-flag=-m --follow-imports --include-module=pgspecial --include-module=psycopg --include-data-files=./pgcli/packages/pgliterals/pgliterals.json=pgcli/packages/pgliterals/pgliterals.json --include-data-files=./pgcli/pgclirc=pgcli/pgclirc
+nuitka --show-progress --onefile -o pgcli-$(uname -o | cut -d / -f 2 | tr 'L' 'l')-musl-$(uname -m).exe pgcli --python-flag=-m --follow-imports --include-module=pgspecial --include-module=psycopg --include-data-files=./pgcli/packages/pgliterals/pgliterals.json=pgcli/packages/pgliterals/pgliterals.json --include-data-files=./pgcli/pgclirc=pgcli/pgclirc
 
 
